f9c39d752843f0d52e5b4fe40999b861d82b3d2a
krosigkm <m.vkrosigk@web.de>
2017-01-02 10:27:42 +0100

    Added empty R and Latex document to work on

---
+++ SEfE_Data_Analysis.R
@@ -0,0 +1,23 @@
+##############################################
+#######    Knoepfel, Krosigk - SEfE    #######
+##############################################
+
+##########
+##    Misc
+##########
+
+# rm(list = ls())
+# setwd("C:\Users\Admin\Google Drive\Uni\HSG\7,610,1.00_Software_Engineering_for_Economists\Project\Best_project_ever_SEfE")
+#library(zoo, package ="zoo")
+#library(xts, package ="xts")
+#library(timeDate)
+library(lmtest, quietly = T)
+library(AER, quietly = T)
+library(dynlm, quietly = T)
+library(tseries, quietly = T)
+library(forecast, quietly = T)
+
+
+###
+# End
+###
