0f48f2cc588855ff811b9966d47edaa4c8747569
Manuel von Krosigk <m.vkrosigk@web.de>
2017-01-08 23:57:21 +0100

    Started reading the data and manipulating it in the program to ensure reproducibility

--- SEfE_Data_Analysis.R
+++ SEfE_Data_Analysis.R
@@ -7,7 +7,7 @@
 ##########
 
 # rm(list = ls())
-# setwd("C:\Users\Admin\Google Drive\Uni\HSG\7,610,1.00_Software_Engineering_for_Economists\Project\Best_project_ever_SEfE")
+# setwd("C:/Users/Admin/Google Drive/Uni/HSG/7,610,1.00 Software Engineering for Economists/Project/Best_project_ever_SEfE")
 #library(zoo, package ="zoo")
 #library(xts, package ="xts")
 #library(timeDate)
@@ -17,6 +17,23 @@ library(dynlm, quietly = T)
 library(tseries, quietly = T)
 library(forecast, quietly = T)
 
+###
+# Read in data
+###
+
+# first read given data and adjust
+data_given                <- read.csv(file="data/SandP_data.csv", header=TRUE, sep=";")
+Date                      <- as.Date(data_given[,1]) 
+
+# check whether all NA values in 'Date' are in the end of the vector 
+sum(1:length(which(!is.na(Date)))==which(!is.na(Date)))==length(which(!is.na(Date)))
+# truncate vectors to all values that we actually observed
+Date                      <- Date[1:length(which(!is.na(Date)))]
+SandP                     <- data_given[1:length(Date),2]
+riskfree                  <- data_given[1:length(Date),3]
+
+
+# 'skip' option to start reading from csv in later row
 
 ###
 # End
