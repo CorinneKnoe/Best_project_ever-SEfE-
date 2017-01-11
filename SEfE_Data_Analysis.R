b7f14a485f55f932e21e503c9c0ca09fa780d908
Manuel von Krosigk <m.vkrosigk@web.de>
2017-01-11 13:38:48 +0100

    Downloaded longer time horizons of explanatory variables and read them into the file

--- SEfE_Data_Analysis.R
+++ SEfE_Data_Analysis.R
@@ -44,14 +44,61 @@ for(i in 1:length(riskfree)){
 E_ret                     <- SandP - riskfree
 par(mar = c(4, 4, 0.1, 0.1), cex.lab = 0.95, cex.axis = 0.9,
     mgp = c(2, 0.7, 0), tcl = -0.3)
-plot(Date[2:length(Date)],E_ret, type='l',xlab="Date",ylab="Excess returns in %")
+plot(Date[2:length(Date)], E_ret, type='l',xlab="Date",ylab="Excess returns in %")
 
-# 'skip' option to start reading from csv in later row
+#################### end first chunk
 
+# create data frame of factors
+Factors                   <- data.frame(Date)
 
+# read in first factor
+Factor_1                  <- read.csv(file="data/NASDAQCOM.csv", header=TRUE, sep=",",
+                                      na.strings =".", stringsAsFactors=FALSE)
+Factor_1[,1]              <- as.Date(Factor_1[,1])
+# read in second factor
+Factor_2                  <- read.csv(file="data/VIXCLS.csv", header=TRUE, sep=",",
+                                      na.strings =".", stringsAsFactors=FALSE)
+Factor_2[,1]              <- as.Date(Factor_2[,1])
+# add column with respective values of Factor_1 and Factor_2 (or arbitrary amount of factors)
+for(i in 1:nrow(Factors)){
+  Factors[i,2]                  <- Factor_1[which(Date[i]==Factor_1[,1]),2]
+  Factors[i,3]                  <- Factor_2[which(Date[i]==Factor_2[,1]),2]  
+}
+colnames(Factors)[2]      <- "NASDAQ"
+colnames(Factors)[3]      <- "VIX"
+
+# replace NAs with the last value that is not NA in all columns of data frame
+for(j in 2:ncol(Factors)){
+  for(i in 1:nrow(Factors)){
+    if(is.na(Factors[i,j])==T){
+      Factors[i,j]               <- Factors[i-1,j]
+    }
+  } 
+}
+
+summary(Factors)
+
+#################### end second chunk
+
+###
+# Model
+###
+
+# choose in-sample
 
 
 
 ###
 # End
 ###
+
+# Discard data before and after period of interest
+Factor_1                  <- Factor_1[which(Factor_1[,1]==Date[1]):
+                                        which(Factor_1[,1]==Date[length(Date)]),]
+
+# replace NAs with the last value that is not NA in all columns of data frame
+for(i in 1:nrow(Factor_1)){
+  if(is.na(Factor_1[i,2])==T){
+    Factor_1[i,2]               <- Factor_1[i-1,2]
+  }
+} 
